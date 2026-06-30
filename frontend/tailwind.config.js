daaefc9a911c1802689f10c63a60f75f8ce26e9e
Premananthan Thanoyan <premananthanthanoyan@gmail.com>
2026-06-30 10:25:42 +0530

    some frontend files added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default { 
+    content: ['./index.html','./src/**/*.{js,jsx}'],
+
+    theme: {
+        extend: {
+            colors: { primary: '#0D4715', secondary: '#41644A', background: '#F1F0E9' },
+            boxShadow: { soft: '0 14px 40px rgba(13,71,21,.10)' } 
+        } },
+        
+    plugins: [] 
+};
